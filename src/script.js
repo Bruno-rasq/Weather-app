1f71197b5748fd17a43cbda797cbf81cb8114d77
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-11-20 23:35:03 -0200

    pausa

--- src/script.js
+++ src/script.js
@@ -3,17 +3,24 @@ import { fetchData } from "./services/api.js"
 const btn_search = document.querySelector('#buscar');
 const Input_text = document.querySelector('#input_text');
 
+const CityName = document.querySelector('#CityName');
+const CountryName = document.querySelector('#CountryName');
+const RegionName = document.querySelector('#RegionName');
+
+const img = document.querySelector('#img');
+const temp = document.querySelector('#temp');
+const description_temp = document.querySelector('#description_temp')
+
+
 
 
 btn_search.addEventListener('click', (event) => {
 
     event.preventDefault()
+    let cityResquest = Input_text.value
 
-    let city = Input_text.value
-
-    fetchData(city).then((response) => {
+    fetchData(cityResquest).then((response) => {
         console.log(response)
-        
     })
 
     Input_text.value = ''
